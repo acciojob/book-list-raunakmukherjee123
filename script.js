5b94a53f59b0f8d106b9e78eac2b38dcc9fb27b4
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2024-10-25 17:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
-
-
   const titleInput = document.getElementById("title");
       const authorInput = document.getElementById("author");
       const isbnInput = document.getElementById("isbn");
       const submitBtn = document.getElementById("submit");
       const bookList = document.getElementById("book-list");
-
       submitBtn.addEventListener("click", (event) => {
         event.preventDefault();
-
         const title = titleInput.value;
         const author = authorInput.value;
         const isbn = isbnInput.value;
-
         if (title === "" || author === "" || isbn === "") {
           alert("Please fill up all the fields");
           return;
         }
-
         const row = document.createElement("tr");
-
         row.innerHTML = `
 		<td>${title}</td>
 		<td>${author}</td>
 		<td>${isbn}</td>
 		<td><button class="delete">X</button></td>
 	`;
-		   
-
         bookList.appendChild(row);
-
         titleInput.value = "";
         authorInput.value = "";
-        isbnInput.value = "";
-
-        bookList.addEventListener("click", (e) => {
+        isbnInput.value = "";	  
+      });
+ bookList.addEventListener("click", (e) => {
           if (e.target.classList.contains("delete")) {
             if (confirm("Are you sure you want to remove this book?")) {
               const row = e.target.parentElement.parentElement;
@@ -42,7 +32,5 @@
             }
           }
         });
-		  
-      });
 //e.target is button in table and its parent is <td> and <td> parent is
 //<tr>. So I put that <tr> in row and deleted it.
